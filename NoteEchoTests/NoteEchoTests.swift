ecd3f9bf826a434f48f1cb3907e33e5d6a5c625d
Ame0304 <rhj903265106@gmail.com>
2025-08-05 17:47:15 -0400

    Initial Commit

---
+++ NoteEchoTests/NoteEchoTests.swift
@@ -0,0 +1,16 @@
+//
+//  NoteEchoTests.swift
+//  NoteEchoTests
+//
+//  Created by Vera Ren on 2025-08-05.
+//
+
+import Testing
+
+struct NoteEchoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
